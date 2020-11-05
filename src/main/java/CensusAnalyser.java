0557fc4366f1cfa399952cb234086c6ae3238af9
ShravyaHub <shravyak710@gmail.com>
2020-11-05 18:16:23 +0530

    REFACTORED: created method to extract data from OpenCSV

--- src/main/java/CensusAnalyser.java
+++ src/main/java/CensusAnalyser.java
@@ -9,16 +9,13 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
-public class CensusAnalyser {
+public class CensusAnalyser extends Throwable {
 
     int numberOfRecords;
 
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBeanBuilder<IndianCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndianCensusCSV.class).withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndianCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndianCensusCSV> iterator = csvToBean.iterator();
+            Iterator<IndianCensusCSV> iterator = this.getCSVFileIterator(reader, IndianCensusCSV.class);
             Iterable<IndianCensusCSV> iterable = () -> iterator;
             numberOfRecords = (int) StreamSupport.stream(iterable.spliterator(), false).count();
         } catch (NoSuchFileException noSuchFileException) {
@@ -34,10 +31,7 @@ public class CensusAnalyser {
 
     public int loadStateCodeData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBeanBuilder<StateCodesCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(StateCodesCSV.class).withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<StateCodesCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<StateCodesCSV> iterator = csvToBean.iterator();
+            Iterator<StateCodesCSV> iterator = this.getCSVFileIterator(reader, StateCodesCSV.class);
             Iterable<StateCodesCSV> iterable = () -> iterator;
             numberOfRecords = (int) StreamSupport.stream(iterable.spliterator(), false).count();
         } catch (NoSuchFileException noSuchFileException) {
@@ -45,9 +39,19 @@ public class CensusAnalyser {
                 throw new CensusAnalyserException("Please enter proper file type", CensusAnalyserException.ExceptionType.WRONG_FILE_TYPE);
         } catch (IOException ioException) {
             throw new CensusAnalyserException("Enter proper file path", CensusAnalyserException.ExceptionType.NO_SUCH_FILE);
-        } catch (RuntimeException runtimeException) {
-            throw new CensusAnalyserException("Exception due to incorrect delimiter position", CensusAnalyserException.ExceptionType.NO_SUCH_FIELD);
         }
         return numberOfRecords;
     }
+
+    private <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass).withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
+        } catch (RuntimeException runtimeException) {
+            throw new CensusAnalyserException("Exception due to incorrect delimiter position", CensusAnalyserException.ExceptionType.NO_SUCH_FIELD);
+        }
+    }
+
 }
